f074de1f787c1b4a20fb2a6fbd21db60ac51bbf8
richiaidil <106723859+richiaidil@users.noreply.github.com>
2022-06-09 12:27:05 +0800

    Update body.py

--- body.py
+++ body.py
@@ -11,8 +11,6 @@ st.sidebar.header('Insert the following Parameter/Characteristic')
 
 def user_input_features():
     sast_lvl = st.sidebar.slider('Sastification Level', 0.00, 1.00, 0.50)
-    last_eval = st.sidebar.slider('Last Evaluation', 0.0, 1.0, 0.5)
-    num_proj = st.sidebar.slider('Number of Involved Project', 0, 10, 2)
     comp_time = st.sidebar.slider('Years at company', 0, 10, 2)
     month_hour = st.sidebar.slider('Number of Monthly Hour', 0, 400, 150)
     work_acc = st.sidebar.selectbox('Has involved in any work accident?',(0,1))
@@ -20,8 +18,6 @@ def user_input_features():
     dept = st.sidebar.selectbox('Department',('sales','technical','support','IT','product_mng'))
     salary = st.sidebar.selectbox('Salary Range',('low', 'medium', 'high'))
     data = {'satisfaction_level': sast_lvl,
-            'last_evaluation':last_eval,
-            'number_project':  num_proj,
             'average_montly_hours': month_hour,
             'time_spend_company': comp_time,
             'Work_accident': work_acc,
@@ -38,6 +34,13 @@ st.subheader('The worker''s parameters')
 st.write(df)
 
 worker_data = pd.read_csv("https://raw.githubusercontent.com/richiaidil/HR_analytics_job_prediction/main/HR_comma_sep.csv")
+
+'''
+After check with correlation data between features with target, some features will be removed
+'''
+
+worker_data = worker_data.drop(['last_evaluation','number_project'], axis = 1)
+
 X = worker_data.drop('left',axis=1)
 Y = worker_data['left']
 
